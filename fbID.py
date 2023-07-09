2a21d1f433506d4f59304bce47ceda224dfb01aa
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2023-07-09 17:41:33 +0530

    Update fbID.py

--- fbID.py
+++ fbID.py
@@ -1,6 +1,5 @@
-import xml , os , time
-from xml import etree
-from xml.etree import ElementTree
+import xml , os , time , re
+from bs4 import BeautifulSoup
 
 try:
     import requests as reqs 
@@ -11,15 +10,7 @@ try:
 except ModuleNotFoundError as e :
 
     print("Missing Modules !")
-    minstall = input("\nDo you want to install them ? (yes/no) ")
-    
-    if minstall == "yes" or minstall == "YES" or minstall == "Yes"  :
-        os.system("pip install -r requirements.txt")
-        print("\n Re-Run the program .")
-        input("\nExit >")
-    else :
-        print(e)    
-        input("\nExit >")
+    exit()
 
 banner = """
 █▀▀ ▄▀█ █▀▀ █▀▀ █▄▄ █▀█ █▀█ █▄▀   █ █▀▄
@@ -29,14 +20,11 @@ title = """--------- [+] By GH0STH4CK3R ----------\n"""
 try:
     print(Fore.GREEN + banner)
     print(Fore.LIGHTGREEN_EX + title)
+    extracted_number = ''
 
     print("Example Input :- https://www.facebook.com/ghost.hacker.21 or ghost.hacker.21 \n")
 
     Uname = input("Facebook acount link or username : ")
-
-    def rem_tags (st) :         # Removing html tag function
-
-        return ''.join(xml.etree.ElementTree.fromstring(str(st)).itertext())
 
     if "http" in Uname or "/" in Uname or "www" in Uname :
         url = Uname
@@ -48,30 +36,44 @@ try:
     if resp.status_code == 200 :
 
         data = resp.text
+        
+        soup = BeautifulSoup(data, 'html.parser')
+        meta_tag = soup.find('meta', {'property': 'al:ios:url'})
+        content_value = meta_tag.get('content')
+        meta_tag2 = soup.find('meta', {'property': 'al:android:url'})
+        content_value2 = meta_tag2.get('content')
+        
 
-        id_loc = data.find("fb://profile/")
-        F_id =  data[(id_loc+13):(id_loc+33)]
+        pattern = r'fb://profile/(\d+)'
+        match = re.search(pattern, content_value)
+        match2 = re.search(pattern, content_value2)
 
-        while True :
+        if match:
+            extracted_number = match.group(1)
+            print('\nYour Facebook ID : ',extracted_number)
+        else:
+            if match2 :
+                print('\nYour Facebook ID : ',match2.group(1))
+            else:
+                print("FB ID not found.")
 
-            if F_id.isnumeric() :
-                break
-            F_id = F_id[:-1]
+        if extracted_number != '' :
 
-        page_html = data         
-        page_soup = bSoup(page_html,"html.parser")
-        #GH0STH4CK3R
-        names = page_soup.find("div",{"class":"fsxl fwb"})     
+            url = "https://www.facebook.com/profile.php?id="+extracted_number 
+            response = reqs.get(url, allow_redirects=True)
+            redirected_url = response.url
 
-        print("")
-        print(rem_tags(names))
-        print("\nFacebook id :",F_id)
+            response = reqs.get(redirected_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            meta_tag3 = soup.find('meta', {'name': 'description'})
+            content_value3 = meta_tag3.get('content')
+            print('\nFacebook Profile Name : ',content_value3[0:content_value3.find('is on Facebook')])
 
     else:
         print(Fore.LIGHTRED_EX +"\nEither username doesnt exist or url is incorrect !")
         print("\nError code",resp.status_code)    
 
-except Exception as ee :
+except Exception as ee:
 
     print(Fore.LIGHTRED_EX +"\nSomething went wrong",ee)
 
